a16dfd2ff6750b3214c1de64a9edf638d9612d95
nuricanacar <21060666@stu.omu.edu.tr>
2021-11-10 20:14:13 +0300

    Update helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -1,1 +1,35 @@
-puts "Hello World"
+cities = %w[
+istanbul
+samsun
+"şanlı urfa"
+]
+ puts cities[-0]
+
+exit
+
+puts "adınız nedir"
+
+
+puts "adınız #{gets}"
+
+exit
+exchange_rate = 10.84
+euro = 150
+
+money = exchange_rate * euro
+
+puts money
+
+exit
+
+oran = 18.0 / 100   
+fiyat = 100.58
+
+puts (oran * fiyat).round(2)
+
+exit
+
+kur = 9.32
+dolar = 25
+
+puts tl = kur * dolar
