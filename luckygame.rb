77e7e9b587a509884822e32b7debef37508100c6
nuricanacar <21060666@stu.omu.edu.tr>
2021-11-16 13:17:35 +0300

    .

---
+++ luckygame.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+def gameluck
+  print 'Bir ile kaç arası bir tahmin oyunu oynamak istersiniz? '
+  limit = gets.chomp.to_i
+  print 'Peki kaç deneme hakkınız olsun? '
+  tries = (gets.chomp.to_i)
+  number = rand(limit)+1
+  try = 0
+
+  puts
+  loop do
+    try += 1
+      if try >= tries
+        puts 'Maalesef deneme hakkınız kalmadı'
+        break
+      end
+      print "Tahmininizi giriniz [#{tries}/#{try}] => "
+      guess = gets.chomp.to_i
+  
+    if guess == number
+      puts 'Başardınız'
+    break
+    elsif guess < number
+      puts 'Daha büyük tahmin'
+    elsif guess > limit
+      puts 'Tahmininiz çok saçma!!'
+      break
+    else 
+      puts 'Daha küçük tahmin'
+    end
+  end
+  puts 'Yeni oyunlarda görüşmek üzere'
+end
+
+gameluck
