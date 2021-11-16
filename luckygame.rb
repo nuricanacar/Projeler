891f6ae368b6340b61572f0d57bcc9ebfbdac0e6
nuricanacar <21060666@stu.omu.edu.tr>
2021-11-16 13:46:47 +0300

    ilkle
    Sorun düzeltmesi

--- luckygame.rb
+++ luckygame.rb
@@ -4,30 +4,30 @@ def gameluck
   print 'Bir ile kaç arası bir tahmin oyunu oynamak istersiniz? '
   limit = gets.chomp.to_i
   print 'Peki kaç deneme hakkınız olsun? '
-  tries = (gets.chomp.to_i)
-  number = rand(limit)+1
+  tries = gets.chomp.to_i
+  number = rand(limit) + 1
   try = 1
 
   puts
   loop do
-      if try > tries
-        puts 'Maalesef deneme hakkınız kalmadı'
-        break
-      end
-      print "Tahmininizi giriniz [#{tries}/#{try}] => "
-      guess = gets.chomp.to_i
-      try += 1
+    if try > tries
+      puts 'Maalesef deneme hakkınız kalmadı'
+      break
+    end
 
-  
+    print "Tahmininizi giriniz [#{tries}/#{try}] => "
+    guess = gets.chomp.to_i
+    try += 1
+
     if guess == number
       puts 'Başardınız'
-    break
+      break
     elsif guess < number
       puts 'Daha büyük tahmin'
     elsif guess > limit
       puts 'Tahmininiz çok saçma!!'
       break
-    else 
+    else
       puts 'Daha küçük tahmin'
     end
   end
